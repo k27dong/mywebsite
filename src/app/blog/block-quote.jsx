2ee20ca7f74fd795fd67c8e82cd9320a22c9606e
k27dong <me@kefan.me>
2023-03-21 22:13:35 -0400

    update styles on blockquote

--- src/app/blog/block-quote.jsx
+++ src/app/blog/block-quote.jsx
@@ -1,19 +1,20 @@
 import styled from "styled-components"
 
 const Quote = styled.div`
-  padding: 20px 10px 10px 30px;
   background: #eee;
+  padding: 0px 10px 0px 30px;
   /* margin-bottom: 10px; */
 
   p {
     line-height: 25px;
     margin-bottom: 0px;
+    padding-bottom: 1em;
   }
 `
 
 const BlockQuote = (props) => {
-    console.log(props.children[1])
-  return <Quote>{props.children[1]}</Quote>
+    console.log(props.children)
+  return <Quote>{props.children}</Quote>
 }
 
 export default BlockQuote
