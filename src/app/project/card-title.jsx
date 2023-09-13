2e6ab9b0c7bf9c6b9d24dc60fc0e53875459e5ea
Kevin Dong <me@kefan.me>
2023-09-13 21:10:24 +0000

    Format fix

--- src/app/project/card-title.jsx
+++ src/app/project/card-title.jsx
@@ -1,12 +1,12 @@
-import styled from "styled-components";
-import { BookOutlined } from "@ant-design/icons";
-import { CONST } from "../util";
+import styled from "styled-components"
+import { BookOutlined } from "@ant-design/icons"
+import { CONST } from "../util"
 
 const CardTitleWrapper = styled.div`
   display: flex;
   align-items: flex-start;
   font-size: 18px;
-`;
+`
 
 const UserProject = styled.a`
   margin-left: 4px;
@@ -20,24 +20,20 @@ const UserProject = styled.a`
   &:visited {
     color: ${CONST.COLORS.LINK};
   }
-`;
+`
 
 const ProjectName = styled.span`
   font-weight: 600;
   color: ${CONST.COLORS.LINK};
-`;
+`
 
 const CardTitle = ({ project }) => (
   <CardTitleWrapper>
     <BookOutlined />
-    <UserProject
-      href={project.link}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <UserProject href={project.link} target="_blank" rel="noopener noreferrer">
       {CONST.GITHUB_USERNAME} / <ProjectName>{project.name}</ProjectName>
     </UserProject>
   </CardTitleWrapper>
-);
+)
 
-export default CardTitle;
+export default CardTitle
