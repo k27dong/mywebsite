413aba24ca63073d1f1417effe70834c35a4fe90
Kevin Dong <me@kefan.me>
2023-10-11 22:53:40 +0000

    Format fix

--- app/check_booknote_format.py
+++ app/check_booknote_format.py
@@ -8,9 +8,10 @@ exclude_files = [
 ]
 
 # Complete list of punctuations
-punctuations = r'[.!?。！？”…：；]$'
+punctuations = r"[.!?。！？”…：；]$"
 # Pattern for checking tags like [7]
-tag_pattern = r'\[\d+\]'
+tag_pattern = r"\[\d+\]"
+
 
 def check_quotes(directory, single_file=None):
     if single_file:
@@ -18,23 +19,24 @@ def check_quotes(directory, single_file=None):
     else:
         for root, dirs, files in os.walk(directory):
             for file in files:
-                if file.endswith('.md') and file not in exclude_files:
+                if file.endswith(".md") and file not in exclude_files:
                     process_file(root, file)
+
 
 def process_file(root, file):
     missing_quotes = []
     error_char_quotes = []
     tag_quotes = []
-    with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+    with open(os.path.join(root, file), "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
             line = line.strip()
-            if line.startswith('>>'):
+            if line.startswith(">>"):
                 # Check for missing punctuation
                 if not re.search(punctuations, line):
                     missing_quotes.append(line)
                 # Check for error characters
-                if '￼' in line:
+                if "￼" in line:
                     error_char_quotes.append(line)
                 # Check for tags
                 if re.search(tag_pattern, line):
@@ -56,6 +58,7 @@ def process_file(root, file):
             for quote in tag_quotes:
                 print(quote + "\n")
         print("---------\n")
+
 
 directory = "docs/salt/"
 
