169772c51e9bd72b05192787f3fc7f161a5c8fa2
k27dong <me@kefan.me>
2024-02-06 15:27:29 -0500

    minor modification on block quote styling

--- web/app/blog/block-quote.jsx
+++ web/app/blog/block-quote.jsx
@@ -3,12 +3,14 @@ import styled from "styled-components"
 const Quote = styled.div`
   background: #eee;
   padding: 0px 10px 0px 30px;
+  padding: 10px 10px 10px 30px;
   /* margin-bottom: 10px; */
 
   p {
     line-height: 25px;
     margin-bottom: 0px;
     padding-bottom: 1em;
+    padding-bottom: 0em;
   }
 `
 
