e3564a403689edc4fea864684161755d7c8f9a7d
k27dong <me@kefan.me>
2024-04-14 00:56:56 -0400

    update intro pages

--- web/app/about/cn-intro.jsx
+++ web/app/about/cn-intro.jsx
@@ -1,10 +1,14 @@
 import { AboutTitle, AboutContent, SocialMedia, Signature } from "./font-book"
-import { MyAge } from "../util"
+import { MyAge, CONST } from "../util"
 import Link from "../component/link"
 
 const CnIntro = () => {
   return (
     <>
+      <AboutContent>
+        SWE @ <Link href={CONST.AUTODESK}>Autodesk</Link>。
+      </AboutContent>
+
       <AboutContent>
         我是滑铁卢大学
         <Link href="https://uwaterloo.ca/future-students/programs/computer-engineering">
@@ -12,6 +16,7 @@ const CnIntro = () => {
         </Link>
         的毕业生。
       </AboutContent>
+
       <AboutContent>我今年{MyAge()}岁，住在多伦多。</AboutContent>
       <AboutTitle>联系方式</AboutTitle>
       <SocialMedia />
--- web/app/about/en-intro.jsx
+++ web/app/about/en-intro.jsx
@@ -1,13 +1,20 @@
 import { AboutTitle, AboutContent, SocialMedia } from "./font-book"
-import { MyAge } from "../util"
+import { MyAge, CONST } from "../util"
 import Link from "../component/link"
 
 const EnIntro = () => {
   return (
     <>
       <AboutContent>
-      Software Engineer at <Link href={CONST.AUTODESK}>Autodesk</Link>.
+        Software Engineer at <Link href={CONST.AUTODESK}>Autodesk</Link>.
       </AboutContent>
+
+      <AboutContent>
+        University of Waterloo{" "}
+        <Link href={CONST.UWCE}>Computer Engineering</Link> alumnus, Class of
+        2023.
+      </AboutContent>
+
       <AboutContent>
         I'm {MyAge()} years old, currently living in Toronto, Canada.
       </AboutContent>
