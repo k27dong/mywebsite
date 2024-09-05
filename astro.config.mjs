2afe1b7b560bfe75d79dc296642aecca390d3785
k27dong <me@kefan.me>
2024-09-05 17:45:13 -0400

    integrated tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
 
+import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
 
 export default defineConfig({
   site: "http://10.0.0.114:4321/",
   srcDir: "./web",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
 })
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./web/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jbmono: ["JetBrains Mono", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
